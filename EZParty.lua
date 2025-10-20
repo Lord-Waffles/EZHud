e31f58433ca030f255ec0308ff3096497046daf3
Lord-Waffles <w.carpenter226@gmail.com>
2025-10-20 17:35:24 -0400

    Add files via upload

--- EZParty.lua
+++ EZParty.lua
@@ -28,22 +28,9 @@
 -- Libraries
 local config = require('config')
 ------------------------------------------------------------------------------------------
-local addon_settings = config.load(defaults)
+local addon_settings = config.load()
 local hud_scale = addon_settings.ezparty.hud.scale
 local ezparty = {}
-local events = {
-    reload = true,
-    left_click = true,
-    double_left_click = true,
-    right_click = true,
-    double_right_click = true,
-    middle_click = true,
-    scroll_up = true,
-    scroll_down = true,
-    hover = true,
-    drag = true,
-    right_drag = true
-}
 local screen_width = windower.get_windower_settings().ui_x_res
 local screen_height = windower.get_windower_settings().ui_y_res
 local player_frame = {} 
@@ -52,106 +39,158 @@ local slot_occupancy = {}
 local player_hp = {}
 local player_mp = {}
 local player_tp = {}
-local slot_position = {
-    [1] = {x=screen_width - (500 * hud_scale), y=screen_height - ((75 * 6) * hud_scale)},
-    [2] = {x=screen_width - (500 * hud_scale), y=screen_height - ((75 * 5) * hud_scale)},
-    [3] = {x=screen_width - (500 * hud_scale), y=screen_height - ((75 * 4) * hud_scale)},
-    [4] = {x=screen_width - (500 * hud_scale), y=screen_height - ((75 * 3) * hud_scale)},
-    [5] = {x=screen_width - (500 * hud_scale), y=screen_height - ((75 * 2) * hud_scale)},
-    [6] = {x=screen_width - (500 * hud_scale), y=screen_height - (75 * hud_scale)},
-}
+
+-- Generate slot position table accounting for hud_scale
+local slot_position = {}
+for i = 0, 5 do
+	local current
+	if i == 0 then
+		current = { x = addon_settings.ezparty.hud.x_pos * hud_scale , y = addon_settings.ezparty.hud.y_pos * hud_scale }
+	else 
+		current.y = current.y - addon_settings.ezparty.hud.player_frame.size.y
+		slot_position[i] = { x = current.x , y = current.y }
+	end
+end
 
 function ezparty.init()
 	
-	local player_frame_bg = windower.addon_path .. 'player_frame_bg.png'
-	local player_panel_bg = windower.addon_path .. 'player_panel_bg.png'
+	local frame = addon_settings.ezparty.hud.player_frame
+	local panel = addon_settings.ezparty.hud.player_panel
+	local hp = addon_settings.ezparty.hud.hp
+	local mp = addon_settings.ezparty.hud.mp
+	local tp = addon_settings.ezparty.hud.tp
+	
+	-- Set Scale frame
+	frame.offset.x = frame.offset.x * hud_scale 
+	frame.offset.x = frame.offset.y * hud_scale 
+	frame.size.x = frame.offset.x * hud_scale 
+	frame.size.x = frame.offset.x * hud_scale 
+	
+	-- Set Scale Panel
+	panel.offset.x = panel.offset.x * hud_scale 
+	panel.offset.x = panel.offset.y * hud_scale 
+	panel.size.x = panel.offset.x * hud_scale 
+	panel.size.x = panel.offset.x * hud_scale 
+	
+	-- Set Scale HP
+	hp.offset.x = hp.offset.x * hud_scale 
+	hp.offset.x = hp.offset.y * hud_scale 
+	hp.size.x = hp.offset.x * hud_scale 
+	hp.size.x = hp.offset.x * hud_scale 
+	
+	-- Set Scale MP
+	mp.offset.x = mp.offset.x * hud_scale 
+	mp.offset.x = mp.offset.y * hud_scale 
+	mp.size.x = mp.offset.x * hud_scale 
+	mp.size.x = mp.offset.x * hud_scale 
+	
+	-- Set Scale TP
+	tp.offset.x = tp.offset.x * hud_scale 
+	tp.offset.x = tp.offset.y * hud_scale 
+	tp.size.x = tp.offset.x * hud_scale 
+	tp.size.x = tp.offset.x * hud_scale 
+	
+	--[[
+	local hp_bar = { 
+		path = addon_settings.ezparty.hud.hp_bar.path ,
+		offset = { x = addon_settings.ezparty.hud.hp_bar.offset.x * hud_scale , y = addon_settings.ezparty.hud.hp_bar.offset.y * hud_scale } ,
+		size = { x = addon_settings.ezparty.hud.hp_bar.size.x * hud_scale , y = addon_settings.ezparty.hud.hp_bar.y * hud_scale}
+	}
+	local mp_bar = { 
+		path = addon_settings.ezparty.hud.mp_bar.path ,
+		offset = { x = addon_settings.ezparty.hud.mp_bar.offset.x * hud_scale , y = addon_settings.ezparty.hud.mp_bar.offset.y * hud_scale } ,
+		size = { x = addon_settings.ezparty.hud.mp_bar.x * hud_scale , y = addon_settings.ezparty.hud.mp_bar.y * hud_scale)
+	}
+	local tp_bar = { 
+		path = addon_settings.ezparty.hud.tp_bar.path ,
+		offset = { x = addon_settings.ezparty.hud.tp_bar.offset.x * hud_scale , y = addon_settings.ezparty.hud.tp_bar.offset.y * hud_scale } ,
+		size = { x = addon_settings.ezparty.hud.tp_bar.x * hud_scale , y = addon_settings.ezparty.hud.tp_bar.y * hud_scale)
+	}
+	local pannel offset = { x = addon_settings.ezparty.player_pannel.offset.x * hud_scale , y = addon_settings.ezparty.player_pannel.offset.y * hud_scale }
+	--]]
 	
 	
-	for i = 1, 6 do
+	for i = 0, 5 do
 	    local party = windower.ffxi.get_party()
-		local group_id = i
-		local member = party['p'..i - 1]
 		
 		player_frame[i] = images.new{
-			pos = {x = slot_position[i].x, y = slot_position[i].y},
+			pos = {x = slot_position[i].x + frame.offset.x  , y = slot_position[i].y + frame.offset.y},
 			visible = true,
 			color = {alpha = 255, red = 255, green = 255, blue = 255},
-			size = {width = 500 * hud_scale, height = 75 * hud_scale},
-			texture = {path = player_frame_bg, fit = false},
+			size = frame.size,
+			texture = {path = frame.path, fit = false},
 			repeatable = {x = 1, y = 1},
 			draggable = false,
 			layer = 1,
 		}
 		
 		-- Store the scaled size because why not. There is a bug and IDK WHAT IS GOING ON BUT FUCK PLEASE
-		-- I'm desperate here so lets just be safe and store the scaled size nice and neat and expose its
-		player_frame[i].scaled_size = { width = 500 * hud_scale, height = 75 * hud_scale }
+		
+		player_frame[i].scaled_size = frame.size
 		
 		player_panel[i] = images.new{
-			pos = {x = slot_position[i].x + (38 * hud_scale), y = slot_position[i].y + (2 * hud_scale)},
+			pos = { x = slot_position[i].x + panel.offset.x , y = slot_position[i].y + panel.offset.y } ,
 			visible = true,
 			color = {alpha = 255, red = 255, green = 255, blue = 255},
-			size = {width = 460 * hud_scale, height = 47 * hud_scale},
-			texture = {path = player_panel_bg, fit = false},
+			size = panel.size ,
+			texture = {path = panel.path , fit = false},
 			repeatable = {x = 1, y = 1},
 			draggable = true,
 			layer = 2,
-			group = group_id,
+			group = i,
 		}	
 		
-		player_panel[i].scaled_size = { width = 460 * hud_scale, height = 47 * hud_scale }
+		player_panel[i].scaled_size = panel.size
 		
 		player_hp[i] = images.new{
-			pos = {x = slot_position[i].x + ( 57 * hud_scale ) , y = slot_position[i].y + ( 27 * hud_scale )},
+			pos = {x = slot_position[i].x + hp.offset.x , y = slot_position[i].y + hp.offset.y},
 			visible = true,
 			color = {alpha = 255, red = 255, green = 255, blue = 255},
-			size  = {width = 119 * hud_scale, height = 9 * hud_scale},
-			texture = {path = windower.addon_path .. 'HP_bar.png', fit = false},
+			size  = hp.size,
+			texture = {path = hp.path , fit = false},
 			repeatable = {x = 1, y = 1},
 			draggable = false,
 			layer = 3,
-			group = group_id,
+			group = i,
 		}
 		
-		player_hp[i].scaled_size = { width = 119 * hud_scale, height = 9 * hud_scale }
+		player_hp[i].scaled_size = hp.size
 		
 		player_mp[i] = images.new{
-			pos = {x = slot_position[i].x + (206 * hud_scale) , y = slot_position[i].y + ( 26 * hud_scale )},
+			pos = {x = slot_position[i].x + mp.offset.x , y = slot_position[i].y + mp.offset.y},
 			visible = true,
 			color = {alpha = 255, red = 255, green = 255, blue = 255},
-			size  = {width = 119 * hud_scale , height = 9 * hud_scale},
-			texture = {path = windower.addon_path .. 'MP_bar.png', fit = false},
+			size  = mp.size,
+			texture = {path = mp.path , fit = false},
 			repeatable = {x = 1, y = 1},
 			draggable = false,
 			layer = 3,
-			group = group_id,
+			group = i,
 		}
 		
-		player_mp[i].scaled_size = { width = 119 * hud_scale, height = 9 * hud_scale }
+		player_mp[i].scaled_size = mp.size
 		
 		player_tp[i] = images.new{
-			pos = {x = slot_position[i].x + (355 * hud_scale) , y = slot_position[i].y + ( 26 * hud_scale )},
+			pos = {x = slot_position[i].x + tp.offset.x , y = slot_position[i].y + tp.offset.y},
 			visible = true,
 			color = {alpha = 255, red = 255, green = 255, blue = 255},
-			size  = {width = 119 * hud_scale, height = 9 * hud_scale},
-			texture = {path = windower.addon_path .. 'TP_bar.png', fit = false},
+			size  = tp.size,
+			texture = {path = tp.path , fit = false},
 			repeatable = {x = 1, y = 1},
 			draggable = false,
 			layer = 3,
-			group = group_id,
+			group = i,
 		}
 		
-		player_tp[i].scaled_size = { width = 119 * hud_scale, height = 9 * hud_scale }
+		player_tp[i].scaled_size = tp.size
 		
-		player_frame.index = i
-		slot_occupancy[i] = player_frame
-	
-		player_frame[i]:show()
-		player_panel[i]:show()
-		player_hp[i]:show()
-		player_mp[i]:show()
-		player_tp[i]:show()
+		player_frame.index = i -- Do i even use this anymore?
+		slot_occupancy[i] = player_frame[i]
 		
+		panel.offset.y = panel.offset.y + ( addon_settings.ezparty.hud.player_panel.size.y * hud_scale )
+		hp.offset.y = hp.offset.y + ( addon_settings.ezparty.hud.hp.size.y * hud_scale )
+		mp.offset.y = mp.offset.y + ( addon_settings.ezparty.hud.mp.size.y * hud_scale )
+		tp.offset.y = tp.offset.y + ( addon_settings.ezparty.hud.tp.size.y * hud_scale )
 	
 	end
 	
@@ -160,7 +199,7 @@ function ezparty.init()
 	ezparty.slot_position = slot_position
 	ezparty.hud_scale     = hud_scale
 	ezparty.player_frame  = player_frame
-	ezparty.player_panel = player_panel
+	ezparty.player_panel  = player_panel
 	ezparty.player_hp     = player_hp
 	ezparty.player_mp     = player_mp
 	ezparty.player_tp     = player_tp
